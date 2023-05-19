1e3bcdcafafa47203a2a3fb45a5daf0d97eec7ba
kimgorok <kjyse2@naver.com>
2023-05-19 17:09:24 +0900

    Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,37 +1,61 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [coinsList, setCoinsList] = useState([]);
+  const [myMoney, setMyMoney] = useState(0);
+  const [coinPrice, setCoinPrice] = useState("");
+  const [finalPrice, setFinalPrice] = useState(0);
+
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log(toDo);
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
+    setFinalPrice(myMoney / coinPrice);
   };
 
+  const onChange = (event) => {
+    setMyMoney(event.target.value);
+  };
+
+  const CalCoin = (event) => {
+    setCoinPrice(event.target.value);
+  };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoinsList(json);
+        setLoading(false);
+        setCoinPrice(json[0].quotes.USD.price);
+      });
+  }, []);
   return (
     <div>
-      <h1>나의 투두스 ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="투두를 적어..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>코인 {loading ? "" : `${coinsList.length}개`} </h1>
+      {loading ? (
+        <strong>로딩중...</strong>
+      ) : (
+        <div>
+          <h4>구매하려는 코인을 선택</h4>
+          <select onChange={CalCoin} value={coinPrice}>
+            {coinsList.map((coin) => (
+              <option key={coin.id} value={coin.quotes.USD.price}>
+                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <hr />
+
+          <h2>당신의 자금은 : {myMoney} $</h2>
+          <form onSubmit={onSubmit}>
+            <input type="number" placeholder="자금(달러)" onChange={onChange} />
+            <button>제출</button>
+          </form>
+
+          <hr />
+          <h2>당신은 {finalPrice}개의 코인을 구매 가능합니다.</h2>
+        </div>
+      )}
     </div>
   );
 }
